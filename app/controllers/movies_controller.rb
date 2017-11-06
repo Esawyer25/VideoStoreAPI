56a902b8a83c0d4fcd9d40966bf755eb4006384d
Andrea Chen <andreachen.ac@gmail.com>
2017-11-06 14:38:00 -0800

    Minor changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,11 @@
 class MoviesController < ApplicationController
 
   def index
-    movies = Movies.all
+    movies = Movie.all
 
     render(
     json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
     )
   end
 
-
-  id
-  name
-  registered_at
-  postal_code
-  phone
-  movies_checked_out_count
 end
