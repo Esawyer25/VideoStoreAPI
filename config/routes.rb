c6a1df5c32a7b8e5b049210790b0ccf992ad1091
Eva Sawyer <esawyer25@gmail.com>
2017-11-06 14:38:54 -0800

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/zmog', to: 'application#index', as: 'zmog'
   get '/customers', to: 'customers#index', as: 'customers'
+
+  resources :movies, only: [:index, :show, :create]
+
+
+  
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    # These tests are a little verbose - yours do not need to be
+    # this explicit.
+    it "is a real working route" do
+      get customers_path
+      must_respond_with :success
+    end
+
+
+  end
 end
